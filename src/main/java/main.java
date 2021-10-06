dbdb1cfd969c2dd0dbb6b4b5e3695460fa20b49d
Markus Nimi <markus.nimi@mail.utoronto.ca>
2021-10-06 12:01:05 -0400

    Added directories, added test class and test

---
+++ src/main/java/main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+    public static int testMethod(int n, int m) {
+        return n + m;
+    }
+}
---
+++ src/test/java/testClass.java
@@ -0,0 +1,28 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class testClass {
+    int m;
+    int n;
+
+
+    @Before
+    public void setUp() throws Exception {
+        m = 10;
+        n = 16;
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testTestMethod() {
+        assertEquals(main.testMethod(m, n), 26);
+    }
+}
