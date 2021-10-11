9abbaa252b609618159158cb57b7ced843a47e2f
mchan7841 <mattchan876@gmail.com>
2021-10-11 16:13:47 -0400

    Getting rid of test cases

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Test");
-    }
-    public static int testMethod(int n, int m) {
-        return n + m;
+        System.out.println("Hello World");
     }
 }
