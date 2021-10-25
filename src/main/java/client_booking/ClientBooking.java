01a3fe8535eb47a410a168491eb31b1c923a536f
D-K0 <53953422+D-K0@users.noreply.github.com>
2021-10-25 18:20:30 -0400

    filled out clientBooking

--- src/main/java/client_booking/ClientBooking.java
+++ src/main/java/client_booking/ClientBooking.java
@@ -1,5 +1,12 @@
 package client_booking;
 
+import entities.BookableServiceLocation;
+
+import entities.BookableServiceLocation;
+import entities.TimePeriod;
+import entities.VaccineBatch;
+import entities.Client;
+
 /**
  * This is the Use Case for Client Booking.
  * Every time the use case is needed, a new client_booking.ClientBooking instance is created
@@ -8,7 +15,29 @@ package client_booking;
 
 public class ClientBooking implements ClientBookingInterface{
 
-    public ClientBooking(){
+    public ClientBooking()
+    {}
 
+    public boolean AppointmentBooking(Client client, BookableServiceLocation clinic,
+                                      TimePeriod timePeriod, String vaccineBrand, int appointmentId)
+    {
+        AppointmentBooking book = new AppointmentBooking(client, clinic, timePeriod, vaccineBrand, appointmentId);
+        return book.createAppoinment();
     }
+
+    public boolean AppointmentCancellation(Client client, BookableServiceLocation clinic,
+                                           TimePeriod timePeriod, String vaccineBrand, int appointmentId)
+    {
+        AppointmentCancellation cancel = new AppointmentCancellation(client, clinic, timePeriod, vaccineBrand, appointmentId);
+        return cancel.deleteAppoinment();
+    }
+
+    public boolean AppointmentViewing(Client client, BookableServiceLocation clinic,
+                                      TimePeriod timePeriod, String vaccineBrand, int appointmentId)
+    {
+        AppointmentViewing view = new AppointmentViewing(client, clinic, timePeriod, vaccineBrand, appointmentId);
+        return view.AppointmentDetails();
+    }
+
+
 }
