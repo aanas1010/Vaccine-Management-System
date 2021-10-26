a3240f8da19fb615a550f0054b551d58b29e8478
aanas1010 <aabid.anas@mail.utoronto.ca>
2021-10-26 17:02:28 -0400

    AppointmentBooking with a Vaccine dose assigned by both brand AND expiry date

--- src/main/java/client_booking/AppointmentBooking.java
+++ src/main/java/client_booking/AppointmentBooking.java
@@ -4,6 +4,7 @@ import entities.*;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 
 public class AppointmentBooking {
@@ -21,22 +22,44 @@ public class AppointmentBooking {
         this.appointmentId = id;
     }
 
-    public boolean isAppointmentAvailable(){
-        return ((Integer) this.clinic.getSupplyObj().getAvailableVaccines().get(this.vaccineBrand) > 0) &&
-                (this.timePeriod.getAvailableSlots() > 0);
+    //Check if there is an opening for the specified time period
+    public boolean isTimeslotAvailable(){return this.timePeriod.getAvailableSlots() > 0;}
+
+    //Reserve a vaccine dose for this client IF there is a timeslot available
+    public boolean assignVaccineDose() {
+        if (this.isTimeslotAvailable()) {
+            ArrayList<VaccineBatch> batchList = this.clinic.getSupplyObj().getBatchList();
+            VaccineBatch earliestExpiringVaccine = null;
+            LocalDate earliestDate = this.timePeriod.getDateTime().toLocalDate().plusYears(2);
+            for (VaccineBatch batch : batchList) {
+                if (batch.getBrand().equals(this.vaccineBrand) && !batch.isExpired() && batch.getAvailable() > 0) {
+                    if (batch.getExpiry().isBefore(earliestDate)) {
+                        earliestDate = batch.getExpiry();
+                        earliestExpiringVaccine = batch;
+                    } else {
+                        System.out.println("Sorry, the chosen clinic does not have any " + this.vaccineBrand + " doses");
+                        return false;
+                    }
+                }
+            }
+            if (batchList.size() <= 0 || earliestExpiringVaccine == null) {
+                System.out.println("Sorry, the chosen clinic does not have any " + this.vaccineBrand + " doses");
+                return false;
+            }
+            earliestExpiringVaccine.changeReserve(1);
+            return true;
+        }
+        return false;
     }
 
+    // Create an appointment for this client in the Clinic's system
     public boolean createAppointment() {
-        if(this.isAppointmentAvailable())
-        {
+        if(this.isTimeslotAvailable() && this.assignVaccineDose()) {
             Appointment appointment = new Appointment(this.client, this.timePeriod, this.vaccineBrand, this.appointmentId);
             this.clinic.addAppointment(appointment);
             this.timePeriod.findAndReserveSlot();
-            this.clinic.getSupplyObj().removeDose();
             return true;
-        }
-        else
-        {
+        }else{
             System.out.println("appointment unavailable");
             return false;
         }
