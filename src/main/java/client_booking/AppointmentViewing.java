a9785ebba40756549796843daa6c4e4e3b558035
aanas1010 <aabid.anas@mail.utoronto.ca>
2021-10-29 13:17:54 -0400

    Created the output of AppointmentViewing

--- src/main/java/client_booking/AppointmentViewing.java
+++ src/main/java/client_booking/AppointmentViewing.java
@@ -1,16 +1,69 @@
 package client_booking;
 
+import entities.Appointment;
+import entities.BookableServiceLocation;
+
+import java.net.SocketOption;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+
 public class AppointmentViewing {
 
-    public AppointmentViewing(){
+    private final Appointment appointment;
+    private final BookableServiceLocation clinic;
 
+    public AppointmentViewing(BookableServiceLocation clinic, Appointment appointment)
+    {
+        this.appointment = appointment;
+        this.clinic = clinic;
     }
 
     /*
   TODO: fill out AppointmentDetails
  */
-    public String AppointmentDetails()
+    /*
+    produces a message:
+    "Hello [client name],
+     your appoinment for a [brand] vaccine has been set for:
+     [date] - at  [location]"
+
+     or
+
+     "Hello [client name],
+     you do not have any appointment currently booked."
+     */
+
+    public String appointmentDetails()
     {
-        return "";
+        if (this.appointment.getClient().getHasAppointment())
+            return appointmentBooked_message();
+        else
+            return noAppointmentBooked_message();
+    }
+
+    // message methods
+    private String appointmentBooked_message()
+    {
+
+        String clientName = this.appointment.getClient().getName();
+        String vaccineBrand = this.appointment.getVaccineBrand();
+        String location = Integer.toString((this.clinic.getServiceLocationId())); //adjust the location detail if neccary
+
+        LocalDateTime date = this.appointment.getTimePeriod().getDateTime();
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        String strDate = dateFormat.format(date);
+
+        return "Hello " + clientName + ", \n" +
+                "your appoinment for a " + vaccineBrand + " vaccine has been set for: \n" +
+                strDate + " - at " + location + "\n";
+    }
+
+    private String noAppointmentBooked_message()
+    {
+        String clientName = this.appointment.getClient().getName();
+
+        return "Hello " + clientName + ", \n" +
+                "you do not have any appointment currently booked.";
     }
 }
