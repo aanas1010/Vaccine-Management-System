5bcb3da1f6ad09065f4bc0085a988083a868820e
D-K0 <53953422+D-K0@users.noreply.github.com>
2021-11-01 17:33:15 -0400

    comment test view

--- src/test/java/AppointmentViewingTest.java
+++ src/test/java/AppointmentViewingTest.java
@@ -58,14 +58,14 @@ public class AppointmentViewingTest {
         // appointmentViewing2 = new AppointmentViewing(22, clinic);
     }
 
-    @Test(timeout = 100) // Testing that the selected timeslot is available
-    public void TestAppointmentDetails_noAppointmentExists() {
-        String message_correct = "Hello " + "client1" + ", \n" +
-                "your appoinment for a " + "Pfizer" + " vaccine has been set for: \n" +
-                "strDate" + " - at " + "1" + "\n";
-        String message_method = appointmentViewing2.appointmentDetails();
-        messageassertEquals(message_correct, message_method);
-    }
+    // @Test(timeout = 100) // Testing that the selected timeslot is available
+    // public void TestAppointmentDetails_noAppointmentExists() {
+    //     String message_correct = "Hello " + "client1" + ", \n" +
+    //             "your appoinment for a " + "Pfizer" + " vaccine has been set for: \n" +
+    //             "strDate" + " - at " + "1" + "\n";
+    //     String message_method = appointmentViewing2.appointmentDetails();
+    //     messageassertEquals(message_correct, message_method);
+    // }
 
     // @Test(timeout = 100) // Testing that the selected timeslot is available
     // public void TestAppointmentDetails_appointmentExists() {
