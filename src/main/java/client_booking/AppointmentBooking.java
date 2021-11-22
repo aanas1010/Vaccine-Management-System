18f2da2228cc18850246dd09649ff9d5aa484aa5
D-K0 <53953422+D-K0@users.noreply.github.com>
2021-11-22 16:00:37 -0500

    added javadoc to AppointmentBooking

--- src/main/java/client_booking/AppointmentBooking.java
+++ src/main/java/client_booking/AppointmentBooking.java
@@ -20,7 +20,15 @@ public class AppointmentBooking {
     String vaccineBrand;
     int appointmentId;
 
-
+    /**
+    * creates Use Case for booking appointments.
+    * 
+    * @param client The client booking the appointment
+    * @param clinic The clinic where the appointment is book
+    * @param timePeriod The time period when the appointment is expected to happen
+    * @param vaccineBrand the vaccine brand for this appointment
+    * @param appointmentId the id of this appointment 
+    */
     public AppointmentBooking(User client, ClinicDecorator clinic, TimePeriod timePeriod, String vaccineBrand, int id){
         this.client = client;
         this.clinic = clinic;
@@ -29,7 +37,9 @@ public class AppointmentBooking {
         this.appointmentId = id;
     }
 
-    // Return whether there is an opening for the specified time period
+    /** indicates whether there is an opening for the specified time period
+    * @return true the stime period is available - false if not. 
+    */
     private boolean isTimeslotAvailable(){
         for(TimePeriod timePeriod: clinic.getTimePeriods(this.timePeriod.getDateTime().toLocalDate())) {
             if (timePeriod.getDateTime().equals(this.timePeriod.getDateTime())) {
@@ -39,9 +49,10 @@ public class AppointmentBooking {
         return false;
     }
 
-    // Reserve a vaccine dose for this client IF there is a timeslot available
-    // AND this person doesn't already have an appointment
-    // Return the VaccineBatch in question
+    /** Reserve a vaccine dose for this client IF there is a timeslot available
+    * AND this person doesn't already have an appointment.
+    * @return the VaccineBatch in question
+    */
     public VaccineBatch assignVaccineDose() throws ManagementSystemException {
         if(this.client.getHasAppointment()) {
             throw new ManagementSystemException(ManagementSystemException.CLIENT_ALREADY_HAS_APPOINTMENT);
@@ -65,12 +76,17 @@ public class AppointmentBooking {
         return earliestExpiringVaccine;
     }
 
-    // Check if the appointment ID is unique
+    /** Check if the appointment ID is unique
+    @return true if the id is unique - false otherwise
+    */
     private boolean hasUniqueId() {
         return !clinic.getAppointmentIds().contains(appointmentId);
     }
 
-    // Create an appointment for this client in the Clinic's system
+    /** Create an appointment for this client in the Clinic's system
+    * @return String describing the appointment
+    * @throws ManagementSystemException
+    */
     public String createAppointment() throws ManagementSystemException {
         if(!this.isTimeslotAvailable()) {
             throw new ManagementSystemException(ManagementSystemException.TIME_SLOT_UNAVAILABLE);
